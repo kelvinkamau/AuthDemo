a650b3db8369fecd2ba4bd039084a943cfba7667
Kelvin Kamau <kelvinkamau6808@gmail.com>
2018-05-01 17:32:20 +0300

    added firebase auth in login

--- app/src/main/java/com/example/asus/codeyard/HomeActivity.java
+++ app/src/main/java/com/example/asus/codeyard/HomeActivity.java
@@ -1,13 +1,28 @@
 package com.example.asus.codeyard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomeActivity.this, AuthActivity.class));
+            }
+        });
+
     }
 }
